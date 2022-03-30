73f625f929bfc7f578059487cf7806ba447d11cc
ayt <charadras6@gmail.com>
2022-03-30 18:47:18 +0300

    Initial commit

---
+++ Guides/Testguide.lua
@@ -0,0 +1,10 @@
+﻿RXPGuides.RegisterGuide("Testguide",[[
+<< Alliance
+#name Testguide
+#next RestedXP Alliance 60-70\59-61 Hellfire Peninsula
+
+step
+    .goto Teldrassil,58.7,44.4
+    .accept 456 >> Accept The Balance of Nature
+
+]])
